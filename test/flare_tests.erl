c01b069618ce7d12363f784dadea052d197e86c7
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-07-05 21:21:24 -0400

    Improve test coverage

--- test/flare_tests.erl
+++ test/flare_tests.erl
@@ -15,8 +15,21 @@ flare_test_() ->
 produce_subtest() ->
     Topic = <<"test">>,
     ok = flare_topic:start(Topic),
+    {error, topic_already_stated} = flare_topic:start(Topic),
     [flare:produce(Topic, <<"event1">>) || _ <- lists:seq(1, 10000)],
-    ok = flare_topic:stop(Topic).
+    timer:sleep(1000),
+    flare:produce(Topic, <<"event1">>),
+    timer:sleep(1000),
+    ok = flare_topic:stop(Topic),
+    {error, topic_not_started} = flare_topic:stop(Topic),
+
+    Topic2 = <<"test2">>,
+    ok = flare_topic:start(Topic2),
+    flare:produce(Topic2, <<"event2">>),
+    ok = flare_topic:stop(Topic2),
+
+    Topic3 = <<"test3">>,
+    {error, topic_not_started} = flare:produce(Topic3, <<"event3">>).
 
 % utils
 cleanup() ->
